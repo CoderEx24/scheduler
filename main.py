eda821ca4857f7686f636a5d46226cfc78d9bd3e
CoderEx24 <coderex24@protonmail.com>
2025-06-20 19:42:26 +0300

    ui: main now fetches classroom data

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
+from kivy.properties import ListProperty
 from kivy.app import App
+import requests
 from ui import *
 
+from core.data_models import *
+
 class MainApp(App):
-    pass
+    classrooms = ListProperty()
+
+    def on_start(self):
+        classrooms = requests \
+                        .get('http://127.0.0.1:8000/api/classroom/dump') \
+                        .json()
+
+        self.classrooms = [
+            Course(
+                id=i,
+                code='',
+                name=c['course']['title'],
+                required_room_type='',
+                allowed_instructors=[],
+                session_type='',
+                duration=0,
+            )
+            for i, c in enumerate(classrooms)
+        ]
 
 if __name__ == '__main__':
     MainApp().run()
