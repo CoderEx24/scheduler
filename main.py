d7a9ddd2383af51a178fd7299f826b49f296f2a7
CoderEx24 <coderex24@protonmail.com>
2025-06-21 09:15:21 +0300

    ui: implemented fetching professors

--- main.py
+++ main.py
@@ -7,6 +7,26 @@ from core.data_models import *
 
 class MainApp(App):
     classrooms = ListProperty()
+    professors = ListProperty()
+
+    def fetch_professors(self):
+        try:
+            professors = requests \
+                .get('http://127.0.0.1:8000/api/schedule/preference/dump') \
+                .json()
+        except:
+            pass
+        else:
+            self.professors = [
+                Instructor(
+                    id=p['id'],
+                    name=p['name'],
+                    type=p['type'],
+                    availability_slots=p['availability'],
+                )
+                for p in professors
+            ]
+
 
     def fetch_classrooms(self):
         try:
