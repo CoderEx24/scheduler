5b7dd0cf81b9a258a1e91b1fb539d2c284b254e5
CoderEx24 <coderex24@protonmail.com>
2025-06-21 08:58:23 +0300

    ui: refactor classroom fetching logic

--- main.py
+++ main.py
@@ -8,23 +8,26 @@ from core.data_models import *
 class MainApp(App):
     classrooms = ListProperty()
 
-    def on_start(self):
-        classrooms = requests \
-                        .get('http://127.0.0.1:8000/api/classroom/dump') \
-                        .json()
-
-        self.classrooms = [
-            Course(
-                id=i,
-                code='',
-                name=c['course']['title'],
-                required_room_type='',
-                allowed_instructors=[],
-                session_type='',
-                duration=0,
-            )
-            for i, c in enumerate(classrooms)
-        ]
+    def fetch_classrooms(self):
+        try:
+            classrooms = requests \
+                            .get('http://127.0.0.1:8000/api/classroom/dump') \
+                            .json()
+        except:
+            pass
+        else:
+            self.classrooms = [
+                Course(
+                    id=i,
+                    code='',
+                    name=c['course']['title'],
+                    required_room_type='',
+                    allowed_instructors=[],
+                    session_type='',
+                    duration=0,
+                )
+                for i, c in enumerate(classrooms)
+            ]
 
 if __name__ == '__main__':
     MainApp().run()
